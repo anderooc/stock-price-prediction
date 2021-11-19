46328119809d8e13b158e965ee6fefae7aa4e38c
anderooc <andrew.chang.22@bishops.com>
2021-11-19 06:33:52 -0800

    Fixed numpy array dimensions.

--- dataset.py
+++ dataset.py
@@ -2,7 +2,8 @@ import pandas as pd
 from sklearn import preprocessing
 import numpy as np
 
-historyPoints = int(input())
+# I decided to use past 50 days, can be changed
+historyPoints = 50
 
 def csvToDataset(csv_path):
     data = pd.read_csv(csv_path)
@@ -17,16 +18,19 @@ def csvToDataset(csv_path):
     # ohlcv stands for open, high, low, close, volume
     # ohlcvHistories is normalized
     ohlcv = np.array([normalized[i: i + historyPoints].copy() for i in range(len(normalized) - historyPoints)])
-    nextDayOpenNormalized = np.array([normalized[:, 0][i + historyPoints].copy() for i in range(len(normalized) - historyPoints)])
-    nextDayOpenNormalized = np.expand_dims(nextDayOpenNormalized, -1)
+    dims = ohlcv.shape
+    ohlcv = ohlcv[:, :, 0]
+    nextDayCloseNormalized = np.array([normalized[:, 3][i + historyPoints].copy() for i in range(len(normalized) - historyPoints)])
+    nextDayCloseNormalized = np.expand_dims(nextDayCloseNormalized, -1)
 
-    # Predicting open value with past history points
-    nextDayOpen = np.array([data[:, 0][i + historyPoints].copy() for i in range(len(data) - historyPoints)])
-    nextDayOpen = np.expand_dims(nextDayOpenNormalized, -1)
+    # Predicting close value with past history points
+    nextDayClose = np.array([data[:, 3][i + historyPoints].copy() for i in range(len(data) - historyPoints)])
+
+    nextDayClose = np.expand_dims(nextDayClose, -1)
 
     # yNormalizer scales values at end out of normalization
     yNormalizer = preprocessing.MinMaxScaler()
 
     # Verify there is an equal number of x and y values
-    assert ohlcv.shape[0] == nextDayOpenNormalized.shape[0]
-    return ohlcv, nextDayOpenNormalized, nextDayOpen, yNormalizer
+    assert ohlcv.shape[0] == nextDayCloseNormalized.shape[0]
+    return ohlcv, nextDayCloseNormalized, nextDayClose, yNormalizer
--- model.py
+++ model.py
@@ -1,18 +1,20 @@
 from dataset import csvToDataset, historyPoints
 from sklearn.neural_network import MLPRegressor
-from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 csvPath = input()
-ohlcv, nextDayOpen, unscaledY, yNormalizer = csvToDataset(csvPath)
+ohlcv, nextDayClose, unscaledY, yNormalizer = csvToDataset(csvPath)
 
 # Spltting data into training and testing
-# OHLCV
-trainX, testX, trainY, testY = train_test_split(ohlcv, nextDayOpen, test_size = 0.8, shuffle = False)
+# ohlcv is x value, nextDayOpen is y value
+# shuffle is set to true as default; we want data to be organized by timestamp
+trainX, testX, trainY, testY = train_test_split(ohlcv, nextDayClose.ravel(), test_size = 0.8, shuffle = False)
 
-# Splitting up unscaled values for future comparison
+# Splitting up unscaled y values for future comparison
 n = int(ohlcv.shape[0] * 0.8)
 unscaledYTest = unscaledY[n:]
 
-
+regr = MLPRegressor(max_iter=1000).fit(trainX, trainY)
+regr.predict(testX[:2])
+regr.score(testX, testY)
